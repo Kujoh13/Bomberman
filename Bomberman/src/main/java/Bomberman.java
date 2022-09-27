0236436154cc7613464ab6c6f5d61880ca77ccb1
Kujoh13 <masterthai001@gmail.com>
2022-09-28 01:47:44 +0700

    Add basic objects classes

--- Bomberman/src/main/java/Bomberman.java
+++ Bomberman/src/main/java/Bomberman.java
@@ -14,7 +14,6 @@ public class Bomberman extends Application {
 
     public static final int WIDTH = 20;
     public static final int HEIGHT = 15;
-
     private GraphicsContext gc;
     private Canvas canvas;
     private List<GameObject> movingObjects = new ArrayList<>();
@@ -34,6 +33,7 @@ public class Bomberman extends Application {
         stage.show();
     }
 
+    /** Gameplay, character movement and enemies behaviour. */
     public void update() {
         movingObjects.forEach(GameObject::update);
     }
