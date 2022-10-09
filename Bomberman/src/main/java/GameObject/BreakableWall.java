ad63c09dc1e4c4526200d2d1c4f349085c16bc6b
Kujoh13 <100248145+Kujoh13@users.noreply.github.com>
2022-10-09 23:58:46 +0700

    Add map create by reading png file

---
+++ Bomberman/src/main/java/GameObject/BreakableWall.java
@@ -0,0 +1,14 @@
+package GameObject;
+
+import javafx.scene.image.Image;
+
+public class BreakableWall extends GameObject{
+    public BreakableWall(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
