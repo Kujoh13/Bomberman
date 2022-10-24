ac4cdf1630fe79391231995b89e94e35a8de65ac
Entropt <21020032@vnu.edu.vn>
2022-10-24 21:48:43 +0700

    Fix minor bugs

--- Bomberman/src/main/java/GameObject/MovingObjects/Enemy.java
+++ Bomberman/src/main/java/GameObject/MovingObjects/Enemy.java
@@ -4,6 +4,7 @@ import GameObject.GameObject;
 import javafx.scene.image.Image;
 
 public class Enemy extends GameObject {
+    public int velocity = 6;
     public Enemy(int x, int y, Image img) {
         super(x, y, img);
     }
