90c6a835557e123d24c8726406262151c60233f4
Entropt <21020032@vnu.edu.vn>
2022-11-01 09:41:43 +0700

    Update Explosion.java

--- Bomberman/src/main/java/GameObject/NonMovingObjects/Explosion.java
+++ Bomberman/src/main/java/GameObject/NonMovingObjects/Explosion.java
@@ -19,9 +19,8 @@ public class Explosion extends GameObject {
     public void update() {
         for(GameObject o: Bomberman.movingObjects) {
             if (o instanceof Enemy && o.collision(this)) {
-                if (((Enemy) o).isDead == false) {
+                if (((Enemy) o).isDead == false)
                     Audio.playEffect(Audio.enemy_die);
-                }
                 ((Enemy) o).isDead = true;
             }
         }
